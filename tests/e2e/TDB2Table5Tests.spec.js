85eea5523de89f7cabe596da9cef8c3f16b4ee31
Aashu Krishnan <gpstore31@gmail.com>
2025-03-14 17:18:23 -0500

    Added test cases for adding new records

--- tests/e2e/TDB2Table5Tests.spec.js
+++ tests/e2e/TDB2Table5Tests.spec.js
@@ -43,6 +43,33 @@ it('M24 Helper test', () => {
         });
     });
 });
+
+it('M24 Added Record Test', () => {
+    const m24Helper = new MaddenRosterHelper();
+    m24Helper.load(m24Path).then(() => {
+        const newRecord = m24Helper.file.CVPM.records[0].deepCopyRecord();
+        newRecord.index = 113;
+        newRecord.fields['ASNM'].value = "SmithGeno_113";
+
+        const originalTableLength = m24Helper.file.CVPM.numEntries;
+
+        m24Helper.file.CVPM.addRecord(newRecord);
+
+        m24Helper.save("tests/data/WriteTest_AddedRecordM24_ROSTER-Official").then(() => {
+            const m24Helper2 = new MaddenRosterHelper();
+            m24Helper2.load("tests/data/WriteTest_AddedRecordM24_ROSTER-Official").then(() => {
+                // Ensure the new entry is present
+                expect(m24Helper2.file.CVPM.records.numEntries).to.equal(originalTableLength + 1);
+                expect(m24Helper2.file.CVPM.records.find((record) => {
+                    return record.index === 113;
+                }).fields['ASNM'].value).to.equal("SmithGeno_113");
+
+                // Ensure that the original record remains intact
+                expect(m24Helper2.file.CVPM.records[0].fields['ASNM'].value).to.equal('SmithGeno_112');
+            });
+        });
+    });
+});
  
 
 it('M25 Helper test', () => {
@@ -80,6 +107,33 @@ it('M25 Helper test', () => {
                 });
 
                 expect(armSleeve2.fields['ITAN'].value).to.equal("GearArmSleeve_Full_sleeveLongUnderarmor_normal_White");
+            });
+        });
+    });
+});
+
+it('M25 Added Record Test', () => {
+    const m25Helper = new MaddenRosterHelper();
+    m25Helper.load(m25Path).then(() => {
+        const newRecord = m25Helper.file.PLEX.records[0].deepCopyRecord();
+        newRecord.index = 113;
+        newRecord.fields['ASNM'].value = "SmithGeno_113";
+
+        const originalTableLength = m25Helper.file.PLEX.numEntries;
+
+        m25Helper.file.PLEX.addRecord(newRecord);
+
+        m25Helper.save("tests/data/WriteTest_AddedRecordM25_ROSTER-Official").then(() => {
+            const m25Helper2 = new MaddenRosterHelper();
+            m25Helper2.load("tests/data/WriteTest_AddedRecordM25_ROSTER-Official").then(() => {
+                // Ensure the new entry is present
+                expect(m25Helper2.file.PLEX.records.numEntries).to.equal(originalTableLength + 1);
+                expect(m25Helper2.file.PLEX.records.find((record) => {
+                    return record.index === 113;
+                }).fields['ASNM'].value).to.equal("SmithGeno_113");
+
+                // Ensure that the original record remains intact
+                expect(m25Helper2.file.PLEX.records[0].fields['ASNM'].value).to.equal('SmithGeno_112');
             });
         });
     });
