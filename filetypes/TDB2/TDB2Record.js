4a9a8ad8c8441506cd331319d0aeb0bbc7c425e1
Aashu Krishnan <gpstore31@gmail.com>
2025-03-14 17:02:59 -0500

    Added function to deep copy existing record

--- filetypes/TDB2/TDB2Record.js
+++ filetypes/TDB2/TDB2Record.js
@@ -23,6 +23,63 @@ class TDB2Record {
     getFieldByKey(key) {
         return this._fields[key];
     };
+
+    // Returns a deep copy of the current record
+    deepCopyRecord(record = null, cache = new WeakMap()) {
+        // If this is the highest level call, initialize record to the current record
+        if(record === null)
+        {
+            record = this;
+        }
+        
+        if (typeof record !== 'object') {
+            return record;
+        }
+
+        // Handle Buffer objects (used in TDB2Field.raw)
+        if (record instanceof Buffer) {
+            return Buffer.from(record);
+        }
+
+        // Handle circular references
+        if (cache.has(record)) {
+            return cache.get(record);
+        }
+
+        // Create new instance with proper prototype
+        const proto = Object.getPrototypeOf(record);
+        const copy = Object.create(proto);
+        cache.set(record, copy);
+
+        // Copy all property descriptors
+        const descriptors = Object.getOwnPropertyDescriptors(record);
+        for (const [key, descriptor] of Object.entries(descriptors)) {
+            const newDescriptor = { ...descriptor };
+            if ('value' in descriptor) {
+                newDescriptor.value = this.deepCopyRecord(descriptor.value, cache);
+            }
+            Object.defineProperty(copy, key, newDescriptor);
+        }
+
+        // Re-apply Proxy if it's a TDB2Record
+        if (record instanceof TDB2Record) {
+            return new Proxy(copy, {
+                get: function (target, prop, receiver) {
+                    return target.fields[prop] !== undefined ? target.fields[prop].value : target[prop] !== undefined ? target[prop] : null;
+                },
+                set: function (target, prop, receiver) {
+                    if (target.fields[prop] !== undefined) {
+                        target.fields[prop].value = receiver;
+                    } else {
+                        target[prop] = receiver;
+                    }
+                    return true;
+                }
+            });
+        }
+
+        return copy;
+    }
 };
 
 module.exports = TDB2Record;
