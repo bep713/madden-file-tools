777bb41294542eb6120fd23a259fcb447b464c04
Aashu Krishnan <gpstore31@gmail.com>
2025-03-21 21:54:31 -0500

    Fixed memory leak with deepCopyRecord on records with subtables

--- filetypes/TDB2/TDB2Record.js
+++ filetypes/TDB2/TDB2Record.js
@@ -1,3 +1,6 @@
+const TDB2Table = require('./TDB2Table');
+const TDB2Field = require('./TDB2Field');
+
 class TDB2Record {
     constructor() {
         this[Symbol.toStringTag] = 'TDB2Record';
@@ -25,59 +28,79 @@ class TDB2Record {
     };
 
     // Returns a deep copy of the current record
-    deepCopyRecord(record = null, cache = new WeakMap()) {
-        // If this is the highest level call, initialize record to the current record
-        if(record === null)
-        {
-            record = this;
+    deepCopyRecord(record = null, cache = new WeakMap(), isTopLevel = true) {
+        if (record === null) record = this;
+        if (typeof record !== 'object') return record;
+        if (record instanceof Buffer) return Buffer.from(record);
+        if (cache.has(record)) return cache.get(record);
+    
+        if (record instanceof TDB2Record) {
+            const copy = new TDB2Record();
+            copy._fields = {};
+            cache.set(record, copy);
+            for (const key in record._fields) {
+                copy._fields[key] = this.deepCopyRecord(record._fields[key], cache, false);
+            }
+            copy._index = record._index;
+            if (isTopLevel) {
+                return new Proxy(copy, {
+                    get: function (target, prop, receiver) {
+                        return target._fields[prop] !== undefined ? target._fields[prop].value : target[prop] !== undefined ? target[prop] : null;
+                    },
+                    set: function (target, prop, receiver) {
+                        if (target._fields[prop] !== undefined) {
+                            target._fields[prop].value = receiver;
+                        } else {
+                            target[prop] = receiver;
+                        }
+                        return true;
+                    }
+                });
+            }
+            return copy;
         }
-        
-        if (typeof record !== 'object') {
-            return record;
+    
+        if (record instanceof TDB2Table) {
+            const copy = new TDB2Table();
+            cache.set(record, copy);
+            copy._type = record._type;
+            copy._name = record._name;
+            copy._offset = record._offset;
+            copy._unknown1 = record._unknown1;
+            copy._unknown2 = record._unknown2;
+            copy._records = record._records.map(r => this.deepCopyRecord(r, cache, false));
+            copy._rawKey = record._rawKey;
+            copy._numEntriesRaw = record._numEntriesRaw;
+            copy._isSubTable = record._isSubTable;
+            copy._parentInfo = record._parentInfo;
+            copy._fieldDefinitions = record._fieldDefinitions;
+            return copy;
         }
-
-        // Handle Buffer objects (used in TDB2Field.raw)
-        if (record instanceof Buffer) {
-            return Buffer.from(record);
+    
+        if (record instanceof TDB2Field) {
+            const copy = new TDB2Field();
+            cache.set(record, copy);
+            copy._key = record._key;
+            copy._rawKey = record._rawKey;
+            copy._type = record._type;
+            copy._length = record._length;
+            copy._raw = record._raw ? Buffer.from(record._raw) : null;
+            copy._value = record._value ? this.deepCopyRecord(record._value, cache, false) : null;
+            copy._isChanged = record._isChanged;
+            return copy;
         }
-
-        // Handle circular references
-        if (cache.has(record)) {
-            return cache.get(record);
-        }
-
-        // Create new instance with proper prototype
+    
         const proto = Object.getPrototypeOf(record);
         const copy = Object.create(proto);
         cache.set(record, copy);
-
-        // Copy all property descriptors
         const descriptors = Object.getOwnPropertyDescriptors(record);
         for (const [key, descriptor] of Object.entries(descriptors)) {
             const newDescriptor = { ...descriptor };
             if ('value' in descriptor) {
-                newDescriptor.value = this.deepCopyRecord(descriptor.value, cache);
+                newDescriptor.value = this.deepCopyRecord(descriptor.value, cache, false);
             }
             Object.defineProperty(copy, key, newDescriptor);
         }
-
-        // Re-apply Proxy if it's a TDB2Record
-        if (record instanceof TDB2Record) {
-            return new Proxy(copy, {
-                get: function (target, prop, receiver) {
-                    return target.fields[prop] !== undefined ? target.fields[prop].value : target[prop] !== undefined ? target[prop] : null;
-                },
-                set: function (target, prop, receiver) {
-                    if (target.fields[prop] !== undefined) {
-                        target.fields[prop].value = receiver;
-                    } else {
-                        target[prop] = receiver;
-                    }
-                    return true;
-                }
-            });
-        }
-
         return copy;
     }
 };
